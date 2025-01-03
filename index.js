4b7e203549fe9ee64c3d7f5e9e3324bd32a0b516
Vishwanth V <vishwanth.wise@gmail.com>
2025-01-03 22:38:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 let c = 0;
 
-let min = window.prompt("Enter number of minutes")
-while(isNaN(min)||min==""){min = window.prompt("Enter a valid number of minutes,idiot")}
-let sec = window.prompt("Enter number of seconds:")
-while(isNaN(sec)||sec==""){sec = window.prompt("Enter a valid number of seconds,idiot")}
+let min = window.prompt("Enter number of minutes");
+while (isNaN(min) || min == "") {
+    min = window.prompt("Enter a valid number of minutes, please");
+}
+let sec = window.prompt("Enter number of seconds:");
+while (isNaN(sec) || sec == "") {
+    sec = window.prompt("Enter a valid number of seconds, please");
+}
 
-
-max = Number(min)*60 + Number(sec)
+let max = Number(min) * 60 + Number(sec);
 max = Number(max);
 
+let t;
 
+document.getElementById("play").onclick = function() {
+    t = setInterval(count, 1000);
+};
 
-const time = setInterval(count, 1000);
+document.getElementById("pause").onclick = function() {
+    clearInterval(t);
+};
 
 function count() {
     c = c + 1;
     document.getElementById("dis").innerHTML = c;
-    
+
     if (c >= max) {
         document.getElementById("msg").innerHTML = "Time's up!";
-        clearInterval(time);
+        clearInterval(t);
     }
 }
